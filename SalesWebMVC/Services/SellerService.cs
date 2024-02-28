54b9ab7bed589493269f6d4d18eb96831144a6b6
Telmo Fernandes <telmorf@live.com>
2024-02-28 18:12:12 +0000

    Delete Seller

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -14,15 +14,29 @@ namespace SalesWebMVC.Services
             _context = context;
         }
 
-        public List<Seller> FindAll() 
+        public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
         }
 
-        public void Insert(Seller obj) 
+        public void Insert(Seller obj)
         {
             _context.Add(obj);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
+
     }
 }
